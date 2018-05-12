34f4684b2849f34f06adde5dc40cf83a9b873cbe
jessepeterman <jessepeterman@gmail.com>
2018-05-11 22:08:49 -0700

    trying to fix array search and selection feature for random old and new book

--- app/assets/app.js
+++ app/assets/app.js
@@ -108,21 +108,31 @@ const data = {
 
       return newBook;
     },
-    generateRandomOldBook(books){
+    getOldBooks(books){
       //make temporary array for books is older than or equal to 50 years ago
-
-
+      const oldBooks = books.filter(book => book.year < '1968');
+      return oldBooks;
+    },
+    getNewBooks(books){
+      //make temporary array for books is older than or equal to 50 years ago
+      const newBooks = books.filter(book => book.year > '1968');
+      return newBooks;
+    },
+    generateRandomBook(books){
       // Generate random ID ** make sure to use temporary array length
-      let randomID = Math.floor(Math.random()*books.length);
-
-      console.log(randomID);
-
+      const randomID = Math.floor(Math.random()*books.length);
       // find random ID and return single book
-      function isId(book){
-        return book.id === randomID;
-      }
+      // function isId(book){
+      //   return book.id === randomID;
+      // }
+      // const randomBook = books.find(isId);
 
-      let randomBook = books.find(isId);
+      const randomBook = books.filter((book, index)=>{
+        return book.index === randomID;
+      });
+
+      // instead of searching by ID of new array, search by index?
+      console.log(randomBook);
       return randomBook;
     },
     convertArrayOfObjectsToCSV(args) {
@@ -176,7 +186,7 @@ const UICtrl = (function(){
     hideListBtn: '.hide-list-btn',
     randomBookDisplay: '.book-recommendation-container',
     oldBookDisplay: '#old-book-title',
-    oldBookBodyDiplay: '.old-book-body',
+    oldBookBodyDisplay: '.old-book-body',
     newBookDisplay: '#new-book-title',
     newBookBodyDisplay: '.new-book-body',
     exportCSVBtn: '.export-csv'
@@ -188,7 +198,7 @@ const UICtrl = (function(){
 
       if(books !== null){
       books.forEach(function(book){
-        html += `<li class="list-group-item">
+        html += `<li class="list-group-item mb-3">
         <strong>Book:</strong> ${book.title}<br>
         <strong>Author:</strong> ${book.author}<br>
         <em>Year: ${book.year},
@@ -216,7 +226,7 @@ const UICtrl = (function(){
       li.id = `item-${book.id}`;
 
       li.innerHTML = `
-      <li class="list-group-item">
+      <li class="list-group-item mb-3">
       <strong>Book:</strong> ${book.title}<br>
       <strong>Author:</strong> ${book.author}<br>
       <em>Year: ${book.year},
@@ -251,7 +261,6 @@ const UICtrl = (function(){
     displayOldBookRecommendation(book){
       // document.querySelector(UISelectors.oldBookDisplay).textContent = book.title;
       UICtrl.showRandomBooks();
-
       const p = document.createElement('p');
 
       p.innerHTML = `
@@ -261,9 +270,21 @@ const UICtrl = (function(){
       </p>
       `;
 
-      document.querySelector(UISelectors.oldBookBodyDiplay).innerHTML = p.innerHTML
+      document.querySelector(UISelectors.oldBookBodyDisplay).innerHTML = p.innerHTML
+    },
+    displayNewBookRecommendation(book){
+      // document.querySelector(UISelectors.oldBookDisplay).textContent = book.title;
+      UICtrl.showRandomBooks();
+      const p = document.createElement('p');
 
+      p.innerHTML = `
+      <p><strong>${book.title}</strong><br>${book.author}<br>
+      <em>Year: ${book.year},
+      Pages: ${book.pages}</em>
+      </p>
+      `;
 
+      document.querySelector(UISelectors.newBookBodyDisplay).innerHTML = p.innerHTML;
     },
     getSelectors: function(){
       return UISelectors;
@@ -291,15 +312,15 @@ const App = (function(BookCtrl, StorageCtrl, UICtrl){
     // book generate old and new button
     document.querySelector(UISelectors.generateBookBtn).addEventListener('click', generateBook);
 
-    // Export list button(csv)?
-    // document.querySelector(UISelectors.exportCSVBtn).addEventListener('click', exportCSV);
+    // Export list button(csv)
+    // Thanks to Chris Grimes for this great looking CSV export code: https://halistechnology.com/2015/05/28/use-javascript-to-export-your-data-as-csv/
     document.querySelector(UISelectors.exportCSVBtn).addEventListener('click', downloadCSV);
 
     // features to implement
-    
+
     // add confirm before generating new book "Did you read your last old book first?"
 
-    // Save old and new book in lcoal storage to remind you before generating new one
+    // Save old and new book in local storage to remind you before generating new one
 
 
   }
@@ -335,12 +356,27 @@ const App = (function(BookCtrl, StorageCtrl, UICtrl){
   };
 
   const generateBook = function(){
-    books = StorageCtrl.getBooksFromStorage();
+    const books = StorageCtrl.getBooksFromStorage();
+    // store array of books 50 years old, send that array to generate old book
+    const oldBooks = BookCtrl.getOldBooks(books);
+    const randomOldBook = BookCtrl.generateRandomBook(oldBooks);
+    UICtrl.displayOldBookRecommendation(randomOldBook);
 
-    let randomBook = BookCtrl.generateRandomOldBook(books);
+    // const newBooks = BookCtrl.getNewBooks(books);
+    // const randomNewBook = BookCtrl.generateRandomNewBook(newBooks);
+    // UICtrl.displayNewBookRecommendation(randomNewBook);
+// closure issue?
+    // const newBooks = BookCtrl.getNewBooks(books);
+    // const randomNewBook = BookCtrl.generateRandomBook(newBooks);
 
-    UICtrl.displayOldBookRecommendation(randomBook);
   }
+
+const generateNewBook = function(){
+  const books = StorageCtrl.getBooksFromStorage();
+  const newBooks = BookCtrl.getNewBooks(books);
+  const randomNewBook = BookCtrl.generateRandomBook(newBooks);
+  UICtrl.displayNewBookRecommendation(randomNewBook);
+}
 
     downloadCSV = function(args) {
         var data, filename, link;
